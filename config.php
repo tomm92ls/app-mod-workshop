37cc2d290b6eed7fb226c8fa67e0e301896a807e
Tomm <tlosterzo@gmail.com>
2025-03-20 15:46:48 +0000

    testing

--- config.php
+++ config.php
@@ -1,9 +1,14 @@
 <?php
-// Configurazione del database
-$db_host = 'localhost';
-$db_name = 'image_catalog';
-$db_user = 'root';
-$db_pass = 'veryverystrongpassword';
+// Database configuration with ENV variables. Set default values as well 
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_name = getenv('DB_NAME') ?: 'image_catalog';
+$db_user = getenv('DB_USER') ?: 'appmod-phpapp-user';
+$db_pass = getenv('DB_PASS') ?: 'wrong_password';
+
+echo $db_host;
+echo $db_name;
+echo $db_user;
+echo $db_pass;
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
